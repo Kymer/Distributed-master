2de7b5ad4e8b2e2c851c949bbc4851c2308e3d02
Devian <info@devian.tk>
2015-10-15 01:20:41 +0200

    clean up the code

--- server/connection.js
+++ server/connection.js
@@ -5,25 +5,22 @@ function makeConnection(address){
 	connection._stream.on('disconnect', Meteor.bindEnvironment(function() {
 		Slaves.update({address}, {$set: {connected: false}})
 	}))
-	Slaves.upsert({address}, {$set: {connected: true}})
 
 	connection.call('getOSInfo', (err, result) => {
 		Slaves.upsert({address}, {$set: {os: result}})
 	})
 	connections.set(address, connection)
 
+	Slaves.upsert({address}, {$set: {connected: true}})
 	return connection
 }
 
 Meteor.startup(function() {
-	Slaves.find().forEach(slave => {
-		makeConnection(slave.address)
-	})
+	Slaves.find().forEach(slave => makeConnection(slave.address))
 })
 
 Meteor.methods({
 	register({address}) {
-		console.log("registering", address)
 		makeConnection(address)
 	},
 	showDialog(address) {

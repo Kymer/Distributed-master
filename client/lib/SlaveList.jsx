815da094ab93bf8d115fecaefc5a690108b2e3e9
Devian <info@devian.tk>
2015-10-21 17:14:43 +0200

    refactor slaveInfo to react

--- client/lib/SlaveList.jsx
+++ client/lib/SlaveList.jsx
@@ -1,5 +1,53 @@
 SlaveList = React.createClass({
+	mixins: [ReactMeteorData],
+
+	getMeteorData() {
+	  return {
+	    slaves: Slaves.find({connected: true}).fetch()
+	  }
+	},
+
+	renderSlaves() {
+		return this.data.slaves.map(slave => <SlaveList.Row slave={slave} />)
+	},
+
 	render() {
-		return <h1>hello world</h1>
+		return (
+			<div className="ui container">
+				<div className="ui hidden divider"></div>
+				<h1 className="ui header">Distributed application</h1>
+				<p>Only 113 lines of code!</p>
+				<table className="ui single line table">
+					<thead>
+						<tr>
+							<th>Platform</th>
+							<th colSpan="2">Hostname</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.renderSlaves()}
+					</tbody>
+				</table>
+			</div>
+		)
 	}
 })
+
+class Row extends React.Component {
+	platformIcon(platformName) {
+		console.log(platformName)
+		return platformIcons.get(platformName)
+	}
+
+	render() {
+		return (
+			<tr>
+				<td><i className={"ui " + this.platformIcon(this.props.slave.os.platform) + " icon"}></i></td>
+				<td><a href={"slaveInfo/" + this.props.slave._id}>{this.props.slave.os.hostname}</a></td>
+				<td className="right aligned"><i className="ui right angle icon"></i></td>
+			</tr>
+		)
+	}
+}
+
+SlaveList.Row = Row

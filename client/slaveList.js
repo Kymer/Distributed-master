2918b3a085628a725efc1ab23f21c078730e4a3e
Kymer <kymer.dev@icloud.com>
2015-11-03 11:18:50 +0100

    refactor slaveList using Vue

--- client/slaveList.js
+++ client/slaveList.js
@@ -1,5 +1,20 @@
-Template.slaveList.helpers({
-	slaves() {return Slaves.find({connected: true})}
+Template.slaveList.onRendered(function() {
+	slaveListVM = new Vue({
+		el: this.find('div.ui.container'),
+		data: {
+			slaves: []
+		},
+	   	sync: {
+	   		slaves() {
+	   			return Slaves.find({connected: true})
+	   		}
+	   	},
+		filters: {
+			platformIcon(platform) {return platformIcons.get(platform)},
+			underscore(object, property) {
+				console.log(object, property)
+				return object[property]
+			}
+		}		
+	})
 })
-
-Template.registerHelper('platformIcon', platform => {return platformIcons.get(platform)})
